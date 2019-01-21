f729fee7ba68c8666ae15a739a3f0daac41e8c59
Jaxier <Jaxier@DESKTOP-HEJK7SO>
2019-01-21 16:06:36 +0800

    test

--- ServletContextDemo/src/com/lingzst/servlet/LoginServlet.java
+++ ServletContextDemo/src/com/lingzst/servlet/LoginServlet.java
@@ -1,20 +1,17 @@
 package com.lingzst.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	public LoginServlet() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -22,10 +19,16 @@ public class LoginServlet extends HttpServlet {
 		String passWord = request.getParameter("password");
 		System.out.println("userName "+ userName);
 		
+		PrintWriter pw = response.getWriter();
 		if("admin".equals(userName) && "123".equals(passWord)) {
 			System.out.println("成功");
+			pw.write("login success...");
+			//设置状态吗
+			response.setStatus(302);
+			response.setHeader("Location", "login_success.html");
 		}else {
 			System.out.println("失败");
+			pw.write("失败");	
 		}
 	}
 
