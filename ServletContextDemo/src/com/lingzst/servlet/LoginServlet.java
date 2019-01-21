fd76b0524b315cdf47529f82c035e9fa6ff55a40
Jaxier <Jaxier@DESKTOP-HEJK7SO>
2019-01-21 14:57:12 +0800

    loginservlet

---
+++ ServletContextDemo/src/com/lingzst/servlet/LoginServlet.java
@@ -0,0 +1,38 @@
+package com.lingzst.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String userName = request.getParameter("username");
+		String passWord = request.getParameter("password");
+		System.out.println("userName "+ userName);
+		
+		if("admin".equals(userName) && "123".equals(passWord)) {
+			System.out.println("成功");
+		}else {
+			System.out.println("失败");
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
